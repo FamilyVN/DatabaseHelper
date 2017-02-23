8e2c1242daea3a939fc1db26bb9cc0b60ff83ee3
TuanAnh <vtabk2@gmail.com>
2017-02-23 22:29:51 +0700

    ref 4 add demo

--- app/src/main/java/com/example/tuananh/databasehelper/database/DBHelper.java
+++ app/src/main/java/com/example/tuananh/databasehelper/database/DBHelper.java
@@ -7,18 +7,18 @@ import com.tuananh.databasehelper.DatabaseHelper;
 /**
  * Created by Tuan Anh on 2/4/2017.
  */
-public class DbHelper extends DatabaseHelper {
+public class DBHelper extends DatabaseHelper {
     private static final String DATABASE_NAME = "test.sqlite3";
     private static final int DATABASE_VERSION = 1;
-    private static DbHelper sInstance;
+    private static DBHelper sInstance;
 
-    private DbHelper(Context context) {
+    private DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
-    public static synchronized DbHelper getInstance(Context context) {
+    public static synchronized DBHelper getInstance(Context context) {
         if (sInstance == null) {
-            sInstance = new DbHelper(context.getApplicationContext());
+            sInstance = new DBHelper(context.getApplicationContext());
         }
         return sInstance;
     }
