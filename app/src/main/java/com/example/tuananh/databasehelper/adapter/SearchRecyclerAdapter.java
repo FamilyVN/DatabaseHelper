6489755d1ff8334576eeb88152ed761733d631bb
vtabk2 <vtabk22gmail.com>
2017-02-26 08:21:56 +0700

    ref Task 6 add demo

--- app/src/main/java/com/example/tuananh/databasehelper/adapter/SearchRecyclerAdapter.java
+++ app/src/main/java/com/example/tuananh/databasehelper/adapter/SearchRecyclerAdapter.java
@@ -2,9 +2,12 @@ package com.example.tuananh.databasehelper.adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.tuananh.databasehelper.R;
 import com.example.tuananh.databasehelper.model.ItemSearch;
 
 import java.util.List;
@@ -15,20 +18,25 @@ import java.util.List;
 public class SearchRecyclerAdapter
     extends RecyclerView.Adapter<SearchRecyclerAdapter.SearchViewHolder> {
     private List<ItemSearch> mItemSearchList;
-    private Context mContext;
+    private LayoutInflater mLayoutInflater;
 
     public SearchRecyclerAdapter(Context context, List<ItemSearch> itemSearchList) {
-        mContext = context;
         mItemSearchList = itemSearchList;
+        mLayoutInflater = LayoutInflater.from(context);
     }
 
     @Override
     public SearchViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View itemView = mLayoutInflater.inflate(R.layout.item_search, parent, false);
+        return new SearchViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(SearchViewHolder holder, int position) {
+        ItemSearch itemSearch = mItemSearchList.get(position);
+        holder.mTextViewId.setText(String.valueOf(itemSearch.getId()));
+        holder.mTextViewValueEn.setText(itemSearch.getEnValue());
+        holder.mTextViewValueJa.setText(itemSearch.getJaValue());
     }
 
     @Override
@@ -37,8 +45,13 @@ public class SearchRecyclerAdapter
     }
 
     public static class SearchViewHolder extends RecyclerView.ViewHolder {
+        private TextView mTextViewId, mTextViewValueEn, mTextViewValueJa;
+
         public SearchViewHolder(View itemView) {
             super(itemView);
+            mTextViewId = (TextView) itemView.findViewById(R.id.text_id);
+            mTextViewValueEn = (TextView) itemView.findViewById(R.id.text_value_en);
+            mTextViewValueJa = (TextView) itemView.findViewById(R.id.text_value_ja);
         }
     }
 }
